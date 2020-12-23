ca2ae7a37d2338fd9821eb15c1be634d67af4f02
Jimmy Briggs <jimbrig2011@outlook.com>
2020-12-22 21:44:23 -0500

    Update install-devtools.ps1

--- install-devtools.ps1
+++ install-devtools.ps1
@@ -1,3 +1,13 @@
+# ref: https://github.com/microsoft/winget-cli | https://github.com/microsoft/winget-pkgs
+
 winget install Microsoft.Powershell-Preview
 winget install Microsoft.WindowsTerminalPreview
 winget install Microsoft.PowerToys
+winget install git
+winget install vscode
+winget install python
+winget install 7zip
+winget install GitHub.cli
+winget install R
+winget install RStudio
+winget install rtools
